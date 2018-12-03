8eb1cbed21c34215a6a98562bee87b8ef97ec68d
Huong <admin@Apples-MacBook-Pro.local>
2018-12-03 21:21:20 +0700

    route

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel 2408
                 </div>
 
                 <div class="links">
--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,18 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('trangchu',function(){
+    return $_GET['page'];
+});
+
+
+Route::get('type/page/{p}',function($page){
+    return $page;
+});
+
+Route::get('type/page/{p?}',function($page=1){
+    return $page;
 });
