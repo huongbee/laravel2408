a09c980c99d2b556373073c5f3ff6febf795f8ef
Huong <admin@Apples-MacBook-Pro.local>
2018-12-05 20:34:37 +0700

    route

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,34 @@ Route::get('type/page/{p}',function($page){
     return $page;
 });
 
-Route::get('type/page/{p?}',function($page=1){
+Route::get('type/page/{p?}/{id?}',function($page=1,$id){
     return $page;
+})->name('type')->where('p','[0-9]+');
+
+Route::get('user/{id}/{username}',function($id, $username){
+    return [
+        'id'=>$id,
+        'username'=>$username
+    ];
+})->name('userinfo')->where([
+    'id' => '[0-9]+',
+    'username'=>'[a-zA-Z]+'
+]);
+
+Route::get('test-redirect', function(){
+    // return redirect('type/page/4454');
+    // return redirect()->route('type',12);
+    return redirect()->route('userinfo',[
+        'id'=>14,
+        'username'=>'customer'
+    ]);
+
+});
+
+// Route::match(['get','post','put'],'detail',function(){
+//     return 12121;
+// });
+
+Route::any('detail',function(){
+    return 12121;
 });
